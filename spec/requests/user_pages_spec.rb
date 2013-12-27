839e9d06c71e1f5439f52482faab5c71fd37a4c7
Lucas Lew Thai Siang <lucaslew@icloud.com>
2013-12-27 15:11:30 +0800

    finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -47,4 +47,13 @@ describe "UserPages" do
     end
   end
   
+  describe "after saving the user" do
+    before { click_button submit }
+    let(:user) { User.find_by(email: 'user@example.com') }
+
+    it { should have_link('Sign out') }
+    it { should have_title(user.name) }
+    it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+  end
+
 end
